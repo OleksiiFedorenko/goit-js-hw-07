40ada76e84e7ad90ad7a19ad7be988e809dd8f84
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-02-05 21:40:43 +0200

    Starting task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+galleryRef.addEventListener('click', onZoomImage);
+
+function onZoomImage(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) return;
+  //   console.log(evt.target.alt);
+  const lightboxInstance = new SimpleLightbox('.gallery a');
+  lightboxInstance.on('show.simplelightbox', function () {
+    // do something…
+  });
+
+  // Options
+  //     {
+  //     captions: true,
+  //     captionSelector: () => evt.target.alt,
+  //     captionType: 'text',
+  //     captionsData: 'atl',
+  //     captionPosition: 'bottom',
+  //     captionDelay: 5,
+  //   }
+}
+
+function createGalleryItemsMarkup(galleryArr = []) {
+  return galleryArr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+}
